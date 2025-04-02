b00736fd2f9a8f7b906ca4c585af301f4c31f9db
Pavel Klos <pavelkloscz@gmail.com>
2025-04-02 08:10:48 +0200

    031-Strings.py

---
+++ 03-basics-1/031-Strings.py
@@ -0,0 +1,36 @@
+# 031 : Strings
+# - data type that is a collection of characters (piece of text)
+# - created by enclosing characters in quotes
+# - single quotes, double quotes, or triple quotes
+
+some_string = "hello"
+print(some_string)
+print(type(some_string))  # <class 'str'>
+user_name = "john"
+password = "password123"
+print(user_name, password)
+long_string = """
+WOW
+O O
+---
+'''
+"""
+print(long_string)
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# - strings are immutable
+# - strings are sequences
+# - strings are iterable
+# - strings are subscriptable
+# - strings are indexable
+# - strings are ordered
+# - strings are collections
+# - strings are objects
+# - strings are instances
+# - strings are classes
+# - strings are data types
+# - strings are values
+# - strings are variables
