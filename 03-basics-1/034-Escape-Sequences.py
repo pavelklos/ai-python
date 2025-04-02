48f140ac93df01d32c3259ea24d992705a6bea02
Pavel Klos <pavelkloscz@gmail.com>
2025-04-02 11:23:46 +0200

    034-Escape-Sequences.py

---
+++ 03-basics-1/034-Escape-Sequences.py
@@ -0,0 +1,28 @@
+# 034 : Escape Sequences
+# - is used to escape a character
+
+weather = "It's a sunny day"  # It's a sunny day
+print(weather)
+weather = "It's a sunny day"  # It's a sunny day
+weather = 'It\'s a "sunny" day'  # It's a "sunny" day
+print(weather)
+weather = "It's a \nsunny day"  # It's a \n sunny day
+print(weather)
+
+# - \n is used to print a new line
+# - \t is used to print a tab
+# - \\ is used to print a backslash
+# - \' is used to print a single quote
+# - \" is used to print a double quote
+# - \b is used to print a backspace
+# - \r is used to print a carriage return
+# - \f is used to print a form feed
+# - \ooo is used to print a character from octal value
+# - \xhh is used to print a character from hexadecimal value
+# - \uhhhh is used to print a character from Unicode
+# - \N{name} is used to print a character from Unicode database
+# - \uxxxx is used to print a character from Unicode
+# - \Uxxxxxxxx is used to print a character from Unicode
+# - \v is used to print a vertical tab
+# - \0 is used to print a null character
+# - \a is used to print an alert
--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 - 031 Strings [.py](03-basics-1/031-Strings.py)
 - 032 String Concatenation [.py](03-basics-1/032-String-Concatenation.py)
 - 033 Type Conversion [.py](03-basics-1/033-Type-Conversion.py)
-- `034 - Escape Sequences.py`
+- 034 Escape Sequences [.py](03-basics-1/034-Escape-Sequences.py)
 - `035 - Formatted Strings.py`
 - `036 - String Indexes.py`
 - `037 - Immutability.py`
