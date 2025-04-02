0d54240277999582c704a78453dac99ca3b0e3b9
Pavel Klos <pavelkloscz@gmail.com>
2025-04-02 07:57:32 +0200

    029-Expressions-vs-Statements.py

---
+++ 03-basics-1/029-Expressions-vs-Statements.py
@@ -0,0 +1,35 @@
+# 029 : Expressions vs Statements
+# - important terms in programming
+
+iq = 100  # expression is 100     statement is iq = 100
+user_age = iq / 5  # expression is iq / 5  statement is user_age = iq / 5
+print(user_age)  # expression is 20.0    statement is print(user_age)
+
+# expression: a piece of code that produces a value
+# statement: entire line of code that performs an action
+
+# expression: iq / 5
+# statement: user_age = iq / 5
+# expression: 20.0
+# statement: print(user_age)
+# expressions can be part of statements
+# statements can contain expressions
+# expressions can be statements
+# statements cannot be expressions
+# expressions cannot be statements
+# expressions produce values
+# statements do things
+# expressions are evaluated
+# statements are executed
+# expressions can be combined into statements
+# statements cannot be combined into expressions
+# statements are instructions
+# expressions are calculations
+# expressions are evaluated to produce a value
+# statements are executed to produce a result
+# expressions are used in statements
+# statements are made up of expressions
+# expressions are like nouns
+# statements are like verbs
+# expressions are like ingredients
+# statements are like recipes
